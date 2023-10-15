1325e8b433ebe91a61414d7be19f45248c795576
smithfmt <smithfmt@gmail.com>
2023-10-15 12:53:40 -0400

    instant consultation

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/signup" element={<Sign_Up/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/appointments" element={<InstantConsultation/>}/>
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
             </Routes>
         </BrowserRouter>
     </div>
