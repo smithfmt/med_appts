c14e63faf3749476e0533f44a0c4afed1e5de52f
smithfmt <smithfmt@gmail.com>
2023-10-15 12:49:35 -0400

    add InstantConsultation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Sign_Up from './Components/SignUp/SignUp';
 import Login from './Components/Login/Login';
+import InstantConsultation from './InstantConsultationBooking/InstantConsultation';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/signup" element={<Sign_Up/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/appointments" element={<InstantConsultation/>}/>
             </Routes>
         </BrowserRouter>
     </div>
