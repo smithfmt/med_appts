9a143460c96f74bfec75c0d0f9cae1b7dbe75c15
smithfmt <smithfmt@gmail.com>
2023-10-16 04:49:03 -0400

    add SEO

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTkxMjEtc21pdGhm@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTc2NDMtc21pdGhm@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://smithfmt-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://smithfmt-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+export const API_URL = window.location.origin;
+//window.location.hostname === "localhost" ? "https://smithfmt-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://smithfmt-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
 console.log(
     "API_URL :",
     API_URL
